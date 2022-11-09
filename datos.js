b366b791d2dbd02dc731d68b6b04a2f55b0ef813
lx04227@innovaccion.mx <lx04227@innovaccion.mx>
2022-11-08 23:54:59 -0600

    se agrego otra tabla de api en bd html

--- datos.js
+++ datos.js
@@ -11,10 +11,10 @@ const cargarDatos = async() => {
         let mecanismo = "";
 
         datos.phases.forEach( phases=> {
-            candidatos += `
+            mecanismo += `
                 <p class="desc">${phases.phase}</p>
             `;
-            mecanismo += `
+             candidatos+= `
                 <p class="desc">${phases.candidates}</p>
             `;
             
@@ -35,3 +35,44 @@ const cargarDatos = async() => {
 }
 
 cargarDatos();
+
+
+const masdatos = async() =>{
+    try{
+        const res = await fetch('https://api.covidtracking.com/v1/states/info.json')
+        console.log(res);
+
+        if(res.status === 200){
+            const devuelto = await res.json();
+            console.log(devuelto);
+            
+            let estados ='';
+            let tipoResultado = '';
+
+            devuelto.forEach( phases=> {
+                estados += `
+                    <p class="desc2">${phases.state}</p>
+                `;
+                 tipoResultado+= `
+                    <p class="desc2">${phases.totalTestResultsField
+                    }</p>
+                `;
+                
+            });
+            
+            
+            document.getElementById('estados').innerHTML = estados;
+            document.getElementById('notas').innerHTML = tipoResultado;
+
+
+
+        }else{
+            console.log('error')
+        }
+
+
+    }catch(error){
+        console.log(error);
+    }
+}
+masdatos();
